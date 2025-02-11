0bb4accf459538be6fc10ba09754aae377f689e2
Jlin-team <jasperlin410@gmail.com>
2025-02-11 15:09:46 +0800

    Update homework

--- student_assignment.py
+++ student_assignment.py
@@ -20,7 +20,7 @@ def hw02_2(q2_pdf):
         chunk_size=10,
         chunk_overlap=0,
          separators=[
-           r"第 +[一二三四五六七八九十\d+-]+ +(?:章|條)"
+           r"第 +[一二三四五六七八九十\d+-]+ +[章條]"
         ],
          is_separator_regex=True
         )
